a1f89f8566a10e01d31f9834ccf4198f6f3f4e82
HideOnHp <55818538+hideonhp@users.noreply.github.com>
2020-05-17 14:37:05 +0700

    Version 0.1.7
    Full Source

---
+++ app/src/main/java/com/example/apple_ios/docbao/MainActivity/WebSiteObject.java
@@ -0,0 +1,11 @@
+package com.example.apple_ios.docbao.MainActivity;
+
+public class WebSiteObject {
+    public int image;
+    public String tenMenu;
+
+    public WebSiteObject(int image, String tenMenu) {
+        this.image = image;
+        this.tenMenu = tenMenu;
+    }
+}
